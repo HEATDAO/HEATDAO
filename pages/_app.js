ee764febea72d61cc34f407d3b0797f2c26755fc
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-10 00:06:16 -0500

    Styled navigation bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,32 +3,38 @@ import Link from 'next/link'
 import Script from 'next/script'
 import Image from 'next/image'
 
+import '../styles/nav.css'
+
 import heat from '../public/images/heat.png'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <link 
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap"
+      />
       <nav className="border-b">
         <Image src={heat} alt="HeayDAO banner" />
-        <div className="flex mt-4">
+        <div className="p-4 font-bold text-center bg-gray-900">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-10 nav-links">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-pink-500">
-              Sell Digital Asset
+            <a className="mr-10 nav-links">
+              Sell your HEAT
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">
-              My Digital Assets
+            <a className="mr-10 nav-links">
+              My Purchases
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-pink-500">
-              Creator Dashboard
+            <a className="mr-10 nav-links">
+              My Creations
             </a>
           </Link>
         </div>
