e527c8e646eb7b11a501db51f38a72fd56a8e6d5
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-09 23:31:23 -0500

    Added HeatDAO banner image to the project and updated the header

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import Link from 'next/link'
-
 import Script from 'next/script'
+import Image from 'next/image'
+
+import heat from '../public/images/heat.png'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+      <nav className="border-b">
+        <Image src={heat} alt="HeayDAO banner" />
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
