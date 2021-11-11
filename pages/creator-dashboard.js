6fd2b270e45bda21c4661c0b6f9f2718d2cd60e8
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-11 18:31:29 -0500

    Updated import paths for smart contracts

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -7,8 +7,8 @@ import {
   nftmarketaddress, nftaddress
 } from '../config'
 
-import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '/Market.sol/NFTMarket.json'
+import NFT from '/NFT.sol/NFT.json'
 
 export default function CreatorDashboard() {
   const [nfts, setNfts] = useState([])
--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ import {
   nftaddress, nftmarketaddress
 } from '../config'
 
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFT from '/NFT.sol/NFT.json'
+import Market from '/Market.sol/NFTMarket.json'
 
 export default function Home() {
   const [nfts, setNfts] = useState([])
