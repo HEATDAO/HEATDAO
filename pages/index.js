d57ef61e62144e2026dc243ab287163c1cc7b930
milan-shrestha0017 <milanshrestha0017@gmail.com>
2021-11-11 22:46:32 -0500

    Updated path to NFT and NFT Market json files

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ import {
   nftaddress, nftmarketaddress
 } from '../config'
 
-import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
-import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+import NFT from '../NFT.json'
+import Market from '../NFTMarket.json'
 
 export default function Home() {
   const [nfts, setNfts] = useState([])
